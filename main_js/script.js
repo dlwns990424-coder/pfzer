b5fe1a412f93708833ff9358571c9bfce06baad9
dlwns990424-coder <dlwns990424@gmail.com>
2026-07-02 01:27:07 +0900

    완성

--- main_js/script.js
+++ main_js/script.js
@@ -50,3 +50,25 @@ const newsSwiper = new Swiper(".newsSwiper", {
   grabCursor: true,
   speed: 700,
 });
+//헤더 스크롤
+const header = document.querySelector("header");
+
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+  const currentScrollY = window.scrollY;
+
+  if (currentScrollY > 10) {
+    header.classList.add("scrolled");
+  } else {
+    header.classList.remove("scrolled");
+  }
+
+  if (currentScrollY > lastScrollY && currentScrollY > 80) {
+    header.classList.add("hide");
+  } else {
+    header.classList.remove("hide");
+  }
+
+  lastScrollY = currentScrollY;
+});
---
+++ sub/business ability/js/business.js
@@ -0,0 +1,16 @@
+const cards = document.querySelectorAll(".ability_wrap > li");
+
+const observer = new IntersectionObserver(
+  (entries) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.add("show");
+      }
+    });
+  },
+  {
+    threshold: 0.3,
+  },
+);
+
+cards.forEach((card) => observer.observe(card));
